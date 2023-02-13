e18587954a72d13e6e5f47119a232eafaa190e55
minktruong1 <minhpro207@yahoo.com>
2023-02-13 23:25:27 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +11,6 @@ app.get("/", (req, res) => {
     message: "Welcome to contact book application",
   });
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
